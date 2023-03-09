15b5c83aea3a470a56664c15a66dfc8fa8bfaa76
bismarkboateng <bismarkb609@gmail.com>
2023-03-09 03:39:35 +0000

    fixed eslint errors

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,9 @@
 const express = require('express');
+
 const router = express.Router();
 const { getStatus, getStats } = require('../controllers/AppController');
 
-
 router.get('/status', getStatus);
 router.get('/stats', getStats);
-
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 const express = require('express');
-const redis = require('redis');
-const mongoose = require('mongoose')
-const routes = require('./routes')
-const { getStatus, getStats } = require('./controllers/AppController');
-const redisClient = require('./utils/redis');
-
+// const redis = require('redis');
+// const mongoose = require('mongoose');
+const routes = require('./routes');
+// const { getStatus, getStats } = require('./controllers/AppController');
+// const redisClient = require('./utils/redis');
 
 const app = express();
 app.use('/', routes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log("Sever running on port 5000");
-})
+  console.log('Sever running on port 5000');
+});
